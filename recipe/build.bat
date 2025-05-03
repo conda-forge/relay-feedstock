4362e09f63e7aa1f41c2d24b51a8883ccabd0208
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-03 10:40:40 -0700

    fix build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,6 @@
-cd compiler || goto :error
+@echo on
 
-:: Disable unexpected_cfg warning
-echo [lints.rust] >> crates\intern\Cargo.toml || goto :error
-echo unexpected_cfgs = { level = 'warn', check-cfg = ['cfg(memory_consistency_assertions)'] } >> crates\intern\Cargo.toml || goto :error
+cd compiler || goto :error
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
